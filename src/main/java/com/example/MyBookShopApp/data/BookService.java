8bab6668cfe0bab54003b117093d9558e40e57d7
Виталий Сазанов <zzzz95515@gmail.com>
2022-07-11 10:43:02 +0300

    step 4

--- src/main/java/com/example/MyBookShopApp/data/BookService.java
+++ src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -19,27 +19,37 @@ public class BookService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
+
     public List<Book> getBooksData() {
         List<Book> books = jdbcTemplate.query("Select * from books", (ResultSet rs, int rowNum) -> {
             Book book =new Book();
             book.setId(rs.getInt("id"));
-            book.setAuthor(rs.getString("author"));
+            book.setAuthor(rs.getInt("author_id"));
             book.setTitle(rs.getString("title"));
-            book.setPriceOld(rs.getString("priceOld"));
+            book.setPriceOld(rs.getString("price_old"));
             book.setPrice(rs.getString("price"));
             return book;
         });
+        for (Book book: books){
+            List<Author> authors =  jdbcTemplate.query("Select * from authors where authors.id = " + String.valueOf(book.getAuthor()),(ResultSet rs, int rowNum) -> {
+                Author author = new Author();
+                author.setLastName(rs.getString("last_name"));
+                author.setFirstName(rs.getString("first_name"));
+                return author;
+            });
+            book.setAuthorName(authors.get(0).getLastName()+' '+authors.get(0).getFirstName());
+        }
         return new ArrayList<>(books);
     }
-    public List<Author> getAuthors(){
-        List<Author> authors = jdbcTemplate.query("Select * from authors ORDER by author",(ResultSet rs, int rowNum) -> {
-            Author author = new Author();
-            author.setAuthor(rs.getString("author"));
-            return author;
-            });
-        return new ArrayList<>(authors);
+
+    public List<Book> getPopularBookData(){
+        return getBooksData();
     }
 
+    public List<Book> getNewBookData(){
+        return getBooksData();
+    }
 
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,16 @@
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
 
-CREATE TABLE  books(
+CREATE TABLE books(
 id INT AUTO_INCREMENT PRIMARY KEY,
-author VARCHAR(250) NOT NULL,
+author_id INT NOT NULL,
 title VARCHAR(250) NOT NULL,
-priceOld  VARCHAR(250) DEFAULT NULL,
+price_old VARCHAR(250) DEFAULT NULL,
 price VARCHAR(250) DEFAULT NULL
 );
-CREATE TABLE  authors(
-id INT AUTO_INCREMENT PRIMARY KEY,
-author VARCHAR(250) NOT NULL
+
+create table authors (
+                         id INT,
+                         first_name VARCHAR(50),
+                         last_name VARCHAR(50)
 );
